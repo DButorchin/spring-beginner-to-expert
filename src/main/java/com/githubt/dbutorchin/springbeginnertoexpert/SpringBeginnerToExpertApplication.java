886e0046838c37b9527965e5a91fa766fde75b25
Dmitri Butorchin <DButorchin@gmail.com>
2017-11-26 13:31:57 +0200

    Initial project

---
+++ src/main/java/com/githubt/dbutorchin/springbeginnertoexpert/SpringBeginnerToExpertApplication.java
@@ -0,0 +1,12 @@
+package com.githubt.dbutorchin.springbeginnertoexpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeginnerToExpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeginnerToExpertApplication.class, args);
+	}
+}
